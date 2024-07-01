fb50c2de0dcd025f9d045a7a8f1c5c2c83ad63b2
anujamerwade <anujamerwade97@gmail.com>
2024-07-01 13:33:33 -0400

    Add functionality to send news feed via email

--- main.py
+++ main.py
@@ -1,15 +1,31 @@
 import requests
+from send_email import send_email
 
+topic = "tesla"
 api_key = "cf0a9ad9170c42e8a851d22449ec2539"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2024-05-28&sortBy=publishedAt&apiKey=cf0a9ad9170c42e8a851d22449ec2539"
+url = f"https://newsapi.org/v2/everything?q=tesla&topic={topic}&sortBy=publishedAt&language=en&apiKey=cf0a9ad9170c42e8a851d22449ec2539"
 
 # make request
 request = requests.get(url)
 
 # get dict with data
 content = request.json()
+print(content)
 
+
+body = ""
 # access article and description
-for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+for article in content['articles'][:10]:
+    if article['title'] is not None and article['description'] is not None:
+        body = "Subject: News Feed" \
+               + "\n" + body + article['title'] + "\n" \
+               + article['description'] \
+               + "\n" + article['url'] + 2*"\n"
+
+body = body.encode("utf-8")
+# message = f"""\
+# Subject: News Feed from Yahoo Finance
+#
+# Topics and Descriptions: {body}
+# """
+send_email(body)
